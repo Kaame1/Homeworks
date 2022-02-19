29b27886fb30011d31f167a9739025df498316ed
Kaame1 <101297matosyan@gmail.com>
2022-02-19 20:39:51 +0400

    h

--- login.py
+++ login.py
@@ -1,1 +1,8 @@
-open('passwprd.json') as file
+import re 
+import getpass
+nominal_password = r'(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[!@#$%^&*])([A-za-z\d!@#$%^&*]){8,}'
+user_input_password = getpass.getpass()
+if re.search(nominal_password,user_input_password) and user_input_password[0].isupper():
+    print('ok')
+else:
+    print('no')    
