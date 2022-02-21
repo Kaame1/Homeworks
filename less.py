7e96f55cf6a9c3b33ccb690299ca9144049183b6
Kaame1 <101297matosyan@gmail.com>
2022-02-22 03:17:26 +0400

    N

--- less.py
+++ less.py
@@ -1,26 +1,31 @@
 # Passwor
-import time
 import getpass
 import re
 import json
 
-with open('password.json', 'w') as file:
-    file.close()
+with open('password.json') as file:
+    my_dict = json.load(file)
+file.close()    
 
-user_input_login = input('Login---')
 nominal_password = r'(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[!@#$%^&*])([A-za-z\d!@#$%^&*]){8,}'
 
 while True:
-    x = {}
-    user_input_password = getpass.getpass()
-    if (re.search(nominal_password, user_input_password)):
-        x = {'login':user_input_login, 'password':user_input_password}       
+    user_input_login = input('Login---')
+    for i in my_dict:
+        if i == user_input_login:
+            print('Your login is already existing\nplease enter oder login') 
+            continue
+    user_input_password = getpass.getpass()    
+    if (re.search(nominal_password, user_input_password)): 
+        my_dict.setdefault(user_input_login,user_input_password)      
         print('Your password is strong')
-        with open('password.json', 'a') as file:
-            json.dump(x, file)
+        with open('password.json', 'w') as file:
+            json.dump(my_dict, file)
         file.close() 
         break
     else:
         print('Your password is not strong\nplease enter angen')
         continue
-            
+        
+
+
--- login.py
+++ login.py
@@ -1,8 +1,42 @@
-import re 
+# import re 
+# import getpass
+# nominal_password = r'(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[!@#$%^&*])([A-za-z\d!@#$%^&*]){8,}'
+# user_input_password = getpass.getpass()
+# if re.search(nominal_password,user_input_password) and user_input_password[0].isupper():
+#     print('ok')
+# else:
+#     print('no') 
+
+import json
 import getpass
-nominal_password = r'(?=.*[A-Z])(?=.*[a-z])(?=.*[\d])(?=.*[!@#$%^&*])([A-za-z\d!@#$%^&*]){8,}'
-user_input_password = getpass.getpass()
-if re.search(nominal_password,user_input_password) and user_input_password[0].isupper():
-    print('ok')
-else:
-    print('no')    
+
+with open('password.json') as file:
+    my_dict = json.load(file)
+file.close()    
+
+while True:
+    user = input('login--')
+    password = getpass.getpass()
+    if my_dict.get(user) == password:
+        print('you ara wellcom')
+        break         
+    else:
+        print('Your password or login are wrong\nplease enter again')
+        continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
