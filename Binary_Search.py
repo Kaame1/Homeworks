18379660374d0fea1beba033bced213fcf7025c2
Kaame1 <101297matosyan@gmail.com>
2022-03-01 21:54:08 +0400

    H

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,22 +1,17 @@
 # BINARY SEARCH
 
+def binary_search(my_list,my_number):
+    min_list = 0
+    max_list = len(my_list) - 1
+    while min_list <= max_list:
+        mid = min_list + max_list // 2
+        if my_list[mid] == my_number:
+            return mid
+        elif my_list[mid] < my_number:
+            min_list = mid + 1
+        else:
+            max_list = mid - 1
+    return 'yor number not in your lit'        
 x = [1,3,5,9,10,13,14,25,27,34,45,67,123,164,200,230,245,400,699]
 n = int(input('eneter number--'))
-def binary_search(my_list,my_number):
-    try: 
-        mid = len(x)//2 
-        mid_relative = len(x)//2 
-        while True:
-            if my_number == my_list[mid]:
-                return mid
-            if my_number > my_list[mid]:
-                mid = mid + (mid_relative // 2)
-                mid_relative = (mid_relative // 2) + 1
-                continue
-            if my_number < my_list[mid]:      
-                mid = mid - (mid_relative // 2)
-                mid_relative = (mid_relative // 2) + 1
-                continue 
-    except IndexError: 
-        return print('yor number not in your lit')
 print('n<index>==',binary_search(x,n))
